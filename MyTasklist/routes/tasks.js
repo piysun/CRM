080687fb68d2fd4cf083ba5496a5712e7dab36c5
Peeyush Nanhe <indus.peeyush@gmail.com>
2018-01-09 14:40:16 +0530

    as

--- MyTasklist/routes/tasks.js
+++ MyTasklist/routes/tasks.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var mongojs = require('mongojs');
-
-var db = mongojs('mongodb://sweetr:Abcd@1234@ds021689.mlab.com:21689/taskbord', ['tasks']);
-
-
 router.get('/tasks', function (req, res, next) {
-    // res.send('TASK API');
-    db.tasks.find(function (err, tasks) {
-        if (err) {
-            res.send(err)
-        }
-        res.json(tasks);
-    });
+    res.send('TASK API');
 });
 
 module.exports = router;
